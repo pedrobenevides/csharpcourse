eb50de253a628e683bd084cb853eabe513354093
pedrobenevides86 <pedrobenevidesb@gmail.com>
2016-01-10 20:00:16 -0300

    Class 02

---
+++ CSharpCourse.Class02/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpCourse.Class02
+{
+    class Program
+    {
+        private static bool mustStop = false;
+
+        static void Main(string[] args)
+        {
+            var list = List1To10();
+            var greatherThanFive = List1To10().Onde(i => i > 5).ToList(); //Perform fetch.
+            
+            MultipleEnumeration();
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> List1To10()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+            yield return 10;
+        }
+
+        public static IEnumerable<int> List1To10WithBreak()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            if(mustStop)
+                yield break;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+            yield return 10;
+        }
+
+        private static void PerformStateMachine()
+        {
+            var stateMachine = new StateMachine();
+            while (stateMachine.MoveNext())
+            {
+                Console.WriteLine(stateMachine.Current);
+            }
+
+        }
+
+        private static void MultipleEnumeration()
+        {
+            var listBreak1 = List1To10WithBreak();
+
+            foreach (var item in listBreak1)
+            {
+                Console.WriteLine(item);
+            }
+
+            mustStop = true;
+            
+            foreach (var item in listBreak1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Onde<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+                if (predicate(item))
+                    yield return item;
+        } 
+    }
+}
---
+++ CSharpCourse.Class02/StateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CSharpCourse.Class02
+{
+    public class StateMachine : IEnumerator<int>
+    {
+        private int state;
+
+        public StateMachine()
+        {
+            state = 0;
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            switch (state)
+            {
+                case 0: Current = 1; state++; return true;
+                case 1: Current = 2; state++; return true;
+                case 2: Current = 3; state++; return true;
+                case 3: Current = 4; state++; return true;
+                case 4: Current = 5; state++; return true;
+                case 5: Current = 6; state++; return true;
+                case 6: Current = 7; state++; return true;
+                case 7: Current = 8; state++; return true;
+                case 8: Current = 9; state++; return true;
+                case 9: Current = 10; state++; return true;
+                default: return false;
+            }
+        }
+
+        public void Reset()
+        {
+            throw new System.NotSupportedException("You should create a new instance");
+        }
+
+        public int Current { get; private set; }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+    }
+}
